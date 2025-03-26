f9c2913a91b48ccc46bde1ebbe0fc64d40b31626
irohanrajput <itusharsingh27@gmail.com>
2025-03-26 19:05:15 +0530

    added converter

--- airdrop/convert.ts
+++ airdrop/convert.ts
@@ -1,8 +1,23 @@
-import  bs58  from "bs58";
+import bs58 from "bs58";
+import wallet from "./dev-wallet.json"
+
+const toBase58 = (secretKey: Uint8Array): string => {
+    return bs58.encode(secretKey);
+};
+
+const toUint8Array = (base58Key: string): Uint8Array => {
+    return bs58.decode(base58Key);
+};
 
 
-const SECRET_KEY_ARRAY = [0]
-const base58PrivateKey = bs58.encode(SECRET_KEY_ARRAY)
 
-console.log("PRIVATE KEY IN BASE58 FORMAT: ", base58PrivateKey)
 
+const SECRET_KEY_ARRAY = new Uint8Array(wallet.secret_key_arr);
+const base58PrivateKey = toBase58(SECRET_KEY_ARRAY);
+console.log("PRIVATE KEY IN BASE58 FORMAT:", base58PrivateKey);
+
+
+// const base58SecretKey = wallet.secret_key_b58
+
+// const decodedUint8Array = toUint8Array(base58SecretKey);
+// console.log("DECODED SECRET KEY ARRAY:", decodedUint8Array);
